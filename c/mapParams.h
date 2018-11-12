9c2780b2c328bf62fdd87a6a00d3dec430db2be1
plectomusic <plectomusic@gmail.com>
2018-11-12 21:45:32 +1100

    initial commit

---
+++ c/mapParams.h
@@ -0,0 +1,33 @@
+
+
+#ifndef ctrnnParams_h
+#define ctrnnParams_h
+
+#ifdef __cplusplus 
+extern "C" { 
+#endif
+
+#include <stdio.h>
+#include <math.h>
+
+double mapGain(double paramValue);
+
+double mapBias(double paramValue);
+
+double mapWeight(double paramValue);
+
+double mapTimeConstant(double paramValue);
+    
+double mapSineCoefficient(double paramValue);
+
+double mapFrequencyMultiplier(double paramValue);
+
+double sineTransferFunction(double activation, double sineCoefficient, double frequencyMiltiplier);
+    
+double transferFunction(double activation);
+
+#ifdef __cplusplus 
+}
+#endif
+
+#endif /* ctrnnParams_h */
